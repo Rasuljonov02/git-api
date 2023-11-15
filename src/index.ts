8574eae8a8818211cc62a05fdfb5c488d1c8f2d0
Rasuljonov <Rasuljonov@Muhammad>
2023-11-15 17:17:22 +0500

    hedr

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
+import './main.css';
 
-function getUser(username: string) {
-	fetch(`https://api.github.com/users/${username}`)
-		.then((res) => res.json())
-		.then((user) => {
-			console.log('user = ', user);
-		});
-}
+// function getUser(username: string) {
+// 	fetch(`https://api.github.com/users/${username}`)
+// 		.then((res) => res.json())
+// 		.then((user) => {
+// 			console.log('user = ', user);
+// 		});
+// }
 
-function getRepos(username: string) {
-	fetch(`https://api.github.com/users/${username}/repos`)
-		.then((res) => res.json())
-		.then((repos) => {
-			console.log('repos = ', repos);
-		});
-}
+// function getRepos(username: string) {
+// 	fetch(`https://api.github.com/users/${username}/repos`)
+// 		.then((res) => res.json())
+// 		.then((repos) => {
+// 			console.log('repos = ', repos);
+// 		});
+// }
 
-getUser('arslonbekXX');
-getRepos('arslonbekXX');
+// console.log(getUser('arslonbekXX'));
 
-// Fetch and XHR(XMLHttpRequest) and AJAX(Async JavaScript And XML)
+// console.log(getRepos('arslonbekXX'));
+
+/**
+ *Fetch and XHR(XMLHttpRequest) and AJAX(Async JavaScript And XML)
+ *
+ */
+
