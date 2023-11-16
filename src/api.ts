84db61481ebd96ea2cf275f6da341c82e019cb84
Rasuljonov <Rasuljonov@Muhammad>
2023-11-16 09:54:56 +0500

    ran

--- src/api.ts
+++ src/api.ts
@@ -1,25 +1,31 @@
 const qidir: HTMLInputElement = document.querySelector(".qidir")!;
+const Followers: HTMLParagraphElement = document.querySelector(".Followers")!;
+const Following: HTMLParagraphElement = document.querySelector(".Following")!;
+const Repositories: HTMLParagraphElement = document.querySelector(".Repositories")!;
+
+
+
 const https = "https://api.github.com/users/";
 const qidiruvvalue = qidir.value;
 function getUser(username: string) {
 	fetch(`${https}${username}`)
 		.then((res) => res.json())
 		.then((user) => {
-			barchamalumot(user.name, user.avatar_url, user.login, user.bio, user.location,user.name);
+			barchamalumot(user.name, user.avatar_url, user.login, user.bio, user.location,user.followers,user.followinguser,user.public_repos);
 		});
 }
 
-function barchamalumot(params: string, img: string, niki: string, bio: string, lacat: string ,name:string) {
+function barchamalumot(params: string, img: string, niki: string, bio: string, lacat: string ,followers:number,following:number,repositories:number) {
 	const odmnirasm: HTMLImageElement = document.querySelector(".odmnirasm")!;
 	const nikElements: NodeListOf<HTMLParagraphElement> = document.querySelectorAll(".nik");
 	const nikid: HTMLParagraphElement = document.querySelector(".nikid")!;
 	const ozihaqidaMalumot: HTMLParagraphElement = document.querySelector(".bio")!;
 	const turarjoyLacation: HTMLParagraphElement = document.querySelector(".location")!;
-	turarjoyLacation.innerText = `${lacat ? lacat : "Uzbekistan"}`;
+	turarjoyLacation.innerText = `${lacat ? lacat : "null"}`;
 	turarjoyLacation;
-	nikid.innerText = `@${niki ? niki : "Rasuljonov02"}`;
+	nikid.innerText = `@${niki ? niki : "null"}`;
 	console.log(params);
-	ozihaqidaMalumot.innerText = bio ? bio : `Bio yoq`;
+	ozihaqidaMalumot.innerText = bio ? bio : `null`;
 
 	odmnirasm.src = `${
 		img
@@ -27,8 +33,20 @@ function barchamalumot(params: string, img: string, niki: string, bio: string, l
 			: `https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU`
 	}`;
 
+	Followers.innerText = `${followers ? `${followers} Followers`  : "0 Followers"}`;
+	Following.innerText = `${following ? `${following} Following` : "0 Following"}`;
+	Repositories.innerText = `${repositories ? `${repositories} Repositories` : "0 Repositories"}`;
+
+
+
+
+
+
+
+
+
 	nikElements.forEach((nik) => {
-		nik.innerText = params ? params : "Rasuljonov Muhammad";
+		nik.innerText = params ? params : "null";
 	});
 }
 
